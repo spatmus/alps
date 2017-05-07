c83573ac86b2b47ff61893769d1662876fa93182
cmtuser <cmtuser@cmt-mbp-13.local>
2017-05-07 11:54:13 +0300

    Initial Commit

---
+++ Audio1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Audio1
+//
+//  Created by cmtuser on 07/05/2017.
+//  Copyright © 2017 cmtuser. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
